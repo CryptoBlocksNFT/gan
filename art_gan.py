093ddf38ff9ef5135eda7304ac6284e6a65828d4
CryptoBlocksNFT <90620567+CryptoBlocksNFT@users.noreply.github.com>
2021-11-05 04:42:53 -0500

    Update art_gan.py

--- art_gan.py
+++ art_gan.py
@@ -2,7 +2,7 @@ from keras.layers import Input, Reshape, Dropout, Dense, Flatten, BatchNormaliza
 from keras.layers.advanced_activations import LeakyReLU
 from keras.layers.convolutional import UpSampling2D, Conv2D
 from keras.models import Sequential, Model, load_model
-from keras.optimizers import Adam
+from tensorflow.keras.optimizers import Adam
 import numpy as np
 from PIL import Image
 import os
